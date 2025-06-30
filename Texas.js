288acd687ec48883368cd66857770afdb0a0ab31
Isaac Méndez Navarrete <isaacmn2103@gmail..com>
2025-06-30 13:51:40 -0600

    shuffle cards

--- Texas.js
+++ Texas.js
@@ -1,11 +1,11 @@
-// Mapa de valores para comparar
+// mapa para comparar
 const valueMap = {
   "2": 2, "3": 3, "4": 4, "5": 5,
   "6": 6, "7": 7, "8": 8, "9": 9,
   "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14
 };
 
-// Función para crear el mazo
+// crear mazo
 function crearMazo() {
   const valores = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
   const palos = ["♠", "♥", "♦", "♣"];
@@ -18,4 +18,12 @@ function crearMazo() {
   }
 
   return mazo;
+}
+
+//  barajear
+function barajar(mazo) {
+  for (let i = mazo.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [mazo[i], mazo[j]] = [mazo[j], mazo[i]]; // intercambia cartas
+  }
 }
